7b06ec40be0a09b95b0b37ee2dfa399cbf31d83b
felypemaciel <felypemaciel674@gmail.com>
2022-05-14 09:33:35 -0300

    atividades de classe 2

---
+++ atividades_classe/NLP/Aula_2/code1.py
@@ -0,0 +1,19 @@
+# Import Counter and word_tokenize
+from nltk import word_tokenize
+from collections import Counter
+
+from nlp_utils import get_sample_article
+
+article = get_sample_article()
+
+# Tokenize the article: tokens
+tokens = word_tokenize(article)
+
+# Convert the tokens into lowercase: lower_tokens
+lower_tokens = [t.lower() for t in tokens]
+
+# Create a Counter with the lowercase tokens: bow_simple
+bow_simple = Counter(lower_tokens)
+
+# Print the 10 most common tokens
+print(bow_simple.most_common(10))
---
+++ atividades_classe/NLP/Aula_2/code2.py
@@ -0,0 +1,27 @@
+# Import WordNetLemmatizer and Counter
+from nltk.stem import WordNetLemmatizer
+from collections import Counter
+
+from nlp_utils import get_wiki_article_lower_tokens, get_english_stop_words
+
+
+lower_tokens = get_wiki_article_lower_tokens()
+
+# Retain alphabetic words: alpha_only
+alpha_only = [t for t in lower_tokens if t.isalpha()]
+
+english_stop = get_english_stop_words()
+# Remove all stop words: no_stops
+no_stops = [t for t in alpha_only if t not in english_stop]
+
+# Instantiate the WordNetLemmatizer
+wordnet_lemmatizer = WordNetLemmatizer()
+
+# Lemmatize all tokens into a new list: lemmatized
+lemmatized = [wordnet_lemmatizer.lemmatize(t) for t in no_stops]
+
+# Create the bag-of-words: bow
+bow = Counter(lemmatized)
+
+# Print the 10 most common tokens
+bow.most_common(10)
